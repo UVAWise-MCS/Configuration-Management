aaf4886a588b2903a8bb7a6e454807320b106a4a
InstantPudding <jpl4k@uvawise.edu>
2015-04-29 13:24:34 -0400

    Added modified yet incomplete files

--- src/java/CommandLine/cmd.java
+++ src/java/CommandLine/cmd.java
@@ -7,22 +7,72 @@ package CommandLine;
 *
 *
 * This file provides the functionality
-* for reading the commands from the 
+* for reading the commands from the
 * console.
 *
 ******************************************/
 
-import java.io.*;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class cmd{
-	public static void main (String[] args){
-		for (String s: args){
-			System.out.println(s);
+	File exam = null;
+	File examAttr = null;
+	Desktop curDesk = null;
+	static String option = null;
+
+	public static void main(String[] args){
+
+		while (option != "3" || option.toLowerCase() != "exit"){
+			System.out.println("Please select an option from" +
+					"on of the following:");
+			option = new String (printMenu());
+
+			if (option == "1" || option.toLowerCase() == "load file"){
+				grabfile();
+			}
+			else if (option == "2" || option.toLowerCase() == "help"){
+				File help = new File("help.md");
+				FileInpuStream fis = new FieInputStream(help);
+				byte[] data = new byte[(int) help.length()];
+				fis.read(data);
+				fis.close();
+
+				String str = new String(data, "UTF-8");
+				System.out.print(str);
+			}
+
 		}
+		System.exit(0);
 	}
 
+	public static String printMenu(){
 
+		Scanner in = new Scanner(System.in);
+		String read = new String();
+
+		System.out.println("1. Load File");
+		System.out.println("2. Help");
+		System.out.println("3. Exit");
+
+		return read = in.nextLine();
+
+	}
+
+	private String grabFile(){
+
+		Scanner in = nw Scanner(System.in);
+		File file = new File(in.nextline());
+
+		try(BufferedReader br = new Buffered Reader(new FileReader(file))){
+			Str
+		}
+
+
+	}
 }
-
-
-
