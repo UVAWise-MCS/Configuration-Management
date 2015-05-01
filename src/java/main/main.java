cbaac8560e2a18df493ade5e9ac81687733b7fde
InstantPudding <jpl4k@uvawise.edu>
2015-04-30 22:46:55 -0400

    Add Command Interpreter implementation.

--- src/java/main/main.java
+++ src/java/main/main.java
@@ -2,19 +2,16 @@
 * Date Written: 02/03/2015
 * Written By: Joseph LaCava
 *
-* DO NOT MODIFY SUPER IMPORTANT
 */
 
 package main;
 
-import java.io.*;
+import CommandLine.Interpreter;
 
 public class main{
-
 	public static void main(String args[]){
-		System.out.println("If you are reading this, know that you are wasting your life.");
+		new Interpreter(args);
 	}
-
 }
 
 
